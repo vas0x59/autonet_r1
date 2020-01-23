e6be6c12cb6d8aa292489c450361d0d9102f221f
vas59 <vasily.yuryev.a1@gmail.com>
2020-01-23 18:34:09 +0700

    enc

--- test/Motor.py
+++ test/Motor.py
@@ -1,15 +1,22 @@
 import rospy
 from std_msgs.msg import Int16
+from std_msgs.msg import Int32
 
 
 class Motor:
     def __init__(self, n):
         self.n = n
-        self.pub = rospy.Publisher('arduino/m' + str(self.n), Int16, queue_size=10)
+        self.pub = rospy.Publisher('/arduino/m' + str(self.n), Int16, queue_size=10)
         # self.sub = 
-        rospy.Subscriber('arduino/enc' + str(self.n), Int16, self.callback)
+        rospy.Subscriber('/arduino/enc' + str(self.n), Int32, self.callback)
         self.enc = 0
     def callback(self, data):
-        self.enc = data.data
+        if self.n == 2:
+            self.enc = -data.data
+        else:
+            self.enc = data.data
     def set_power(self, m):
-        self.pub.publish(m)
+        if self.n == 1:
+            self.pub.publish(-m)
+        else:
+            self.pub.publish(m)
--- test/test_enc_motor.py
+++ test/test_enc_motor.py
@@ -3,24 +3,28 @@ from std_msgs.msg import Int16
 from Motor import Motor
 import math
 
-rospy.init_node('tester_m', anonymous=True)
-m1 = Motor(1)
-m2 = Motor(2)
 
+print("OK")
+m1 = Motor(1)
+m2 = Motor(2)   
+rospy.init_node('tester_m', anonymous=True)
+print("OK")
 # c = 360
 
 # d = 0.07
 
 # k = 
-p1 = 360
-d = 0.07
 
+d = 4
+p1 = 3
 def conv(c):
-    return (c/p1) * (math.pi*d)
+    return (c*p1) 
 
-m1.set_power(10)
-s = conv(m1.enc)
-while conv(m1.enc) < s + 1:
+m2.set_power(10)
+s = m2.enc
+# rospy.sleep(2)
+while m2.enc < s + 1427*7:
     rospy.sleep(0.01)
-m1.set_power(0)
+    m2.set_power(10)
+m2.set_power(0)
 
