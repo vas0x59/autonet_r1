b1ca148a323fbd43511518e50755a9f8d6d23e40
vas59 <vasily.yuryev.a1@gmail.com>
2020-01-26 23:20:01 +0700

    ocr

--- src/digitRecognition/OCR.py
+++ src/digitRecognition/OCR.py
@@ -2,11 +2,11 @@ import pytesseract
 import cv2
 import numpy as np
 
-class recog:
-    def __init__(self, cam):
-        self.cap = cv2.VideoCapture(cam)
-    def get(self):
-        ret, img = self.cap.read()
+class NumReg:
+    def __init__(self, numbers="0123456789"):
+        self.numbers = numbers
+    def get(self, img):
         img = cv2.bitwise_not(img)
-        text = pytesseract.image_to_string(img, config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
-        return text
+        text = pytesseract.image_to_string(img, config='--psm 10 --oem 3 -c tessedit_char_whitelist=' + str(self.numbers))
+        color = "none"
+        return text, color
