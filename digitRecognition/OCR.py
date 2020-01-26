ce5b72fe454fc4f2776eda8dbd652f8c0f098eaf
Dmitrii <39788810+MagicWinnie@users.noreply.github.com>
2020-01-26 22:36:41 +0700

    Update OCR.py

--- digitRecognition/OCR.py
+++ digitRecognition/OCR.py
@@ -7,11 +7,15 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, img = cap.read()
-    text = pytesseract.image_to_string(img)
+    #create negative
+    #img = cv2.bitwise_not(img)
+    #create gray
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.bitwise_not(img)
+    text = pytesseract.image_to_string(img, config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
     cv2.imshow("image", img)
     print(text)
     if cv2.waitKey(10) == 27:
         break
 cap.release()
 cv2.destroyAllWindows()
-
