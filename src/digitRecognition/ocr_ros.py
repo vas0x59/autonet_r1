9e5efb1652afe2bd765fdcbdbdbca1e4c61f5113
vas59 <vasily.yuryev.a1@gmail.com>
2020-01-26 23:24:49 +0700

    ros

---
+++ src/digitRecognition/ocr_ros.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+
+from OCR import NumReg
+
+bridge = CvBridge()
+nr = NumReg()
+
+rospy.init_node('ocr', anonymous=True)
+
+ocr_pub_text = rospy.Publisher('/ocr/text', String, queue_size=10)
+ocr_pub_color = rospy.Publisher('/ocr/color', String, queue_size=10)
+
+
+def img_clb(data):
+    # global bridge
+    cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+    text, color = nr.get(cv_image)
+    ocr_pub_text.publish(text)
+    ocr_pub_color.publish(color)
+
+
+image_sub = rospy.Subscriber(
+    "/cam2/image_raw", Image, img_clb)
+# ic = image_converter()
+
+
+try:
+    rospy.spin()
+except KeyboardInterrupt:
+    print("Shutting down")
+# cv2.destroyAllWindows()
