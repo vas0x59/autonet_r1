5d5298330676082c0f3c903cf7382017da9a8b50
vas59 <vasily.yuryev.a1@gmail.com>
2020-01-27 15:34:49 +0700

    python

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['autonet_r1'],
+    package_dir={'': 'src'}
+)
+
+setup(**d)
