eefa29c1274eebcb2faf15c402724ee70b9e92c4
vas59 <vasily.yuryev.a1@gmail.com>
2020-01-28 17:41:31 +0700

    reg line

--- src/lane_detector/lane_ros.py
+++ src/lane_detector/lane_ros.py
@@ -4,18 +4,34 @@ import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
+from autonet_r1.msg import LaneRes
+from reg_line1 import RegLine
 bridge = CvBridge()
 
+image_pub = rospy.Publisher("/lane/debug_img",Image)
+res_pub = rospy.Publisher("/lane/res",LaneRes)
+
+rl = RegLine()
 
 def img_clb(data):
     # global bridge
     cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
-    cv2.imshow("Image window", cv_image)
-    cv2.waitKey(3)
+    out_img = cv_image.copy()
+    # cv2.imshow("Image window", cv_image)
+    # cv2.waitKey(3)
+    e1, e2, out_img, color = rl.reg_line(cv_image)
+    lr_msg = LaneRes()
+    lr_msg.color = str(color)
+    lr_msg.e1 = e1
+    lr_msg.e2 = e2
+    # lr.e1 = 
+    res_pub.publish(lr_msg)
+    image_pub.publish(bridge.cv2_to_imgmsg(out_img, "bgr8"))
 
 
 image_sub = rospy.Subscriber(
     "/cam1/image_raw", Image, img_clb)
+
 # ic = image_converter()
 rospy.init_node('image_converter', anonymous=True)
 
@@ -23,4 +39,4 @@ try:
     rospy.spin()
 except KeyboardInterrupt:
     print("Shutting down")
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
