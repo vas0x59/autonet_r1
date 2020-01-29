2f1e1940c6ca2a90b94fbc0b6cce2107225a3de1
vas59 <vasily.yuryev.a1@gmail.com>
2020-01-29 15:35:30 +0700

    res

--- src/nav/nav.py
+++ src/nav/nav.py
@@ -67,13 +67,13 @@ def set_nav(data):
 
 def get_tem(data):
     global res_x, res_y, res_yaw, tf_buffer
-    res = GetTelemetryResponse()
+    # res = GetTelemetryResponse()
     # res.x =
-    x, y, z = transform_xy_yaw(res_x, res_y, res_yaw, "nav", data.frame, tf_buffer) 
-    res.x = x
-    res.y = y
-    res.yaw = yaw
-    return res
+    x, y, yaw = transform_xy_yaw(res_x, res_y, res_yaw, "nav", data.frame, tf_buffer) 
+    # res.x = x
+    # res.y = y
+    # res.yaw = yaw
+    return {"x":x, "y":y, "yaw":yaw}
 s = rospy.Service('set_nav', SetNav, set_nav)
 gs = rospy.Service('get_telemetry', GetTelemetry, get_tem)
 
