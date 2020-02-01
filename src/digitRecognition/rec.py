4865e4bb2d7eaa12f421b9a1346ec8acade1ac46
Dmitrii <39788810+MagicWinnie@users.noreply.github.com>
2020-02-01 13:12:32 +0700

    Create rec.py

---
+++ src/digitRecognition/rec.py
@@ -0,0 +1,47 @@
+import time
+import cv2
+import numpy as np
+
+from OCR import NumReg
+# from shapeDetector import shapeDetector
+from colorRecognition import colorRec
+
+
+
+
+class rec:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(1)
+        self.cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+        self.i = 0
+        self.filt = ''
+        print("started")
+        self.color = ""
+        self.text = ""
+        
+        self.nr = NumReg()
+        # sd = shapeDetector()
+        self.cr = colorRec()
+    def most_frequent(self, s): 
+        return max(set(s), key = s.count) 
+
+    def recog(self):
+        
+        # while cv2.waitKey(1) != ord('q'):
+        _, frame = self.cap.read()
+        
+        # if self.i == 3:
+        #     print("[DEBUG]", self.text)    
+        #     self.i = 0
+        #     self.filt = ''
+        
+        
+        _, frame = self.cap.read()
+        self.text = self.nr.get(frame, show = True)
+        self.color = self.cr.colorRec(frame)
+        print("[DEBUG]", self.color)
+        # if self.text == '': return 
+        # if int(self.text) > 5: return
+        # self.filt += self.text
+        # self.i += 1
+        return self.text, self.color
