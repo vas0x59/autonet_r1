57f4a045de34c69999ebfb1d03aaa9dc18549c0e
vas59 <vasily.yuryev.a1@gmail.com>
2020-02-01 12:13:34 +0700

    aa

--- src/nav/do_way.py
+++ src/nav/do_way.py
@@ -83,9 +83,9 @@ for point_name in path[2:]:
     x, y = map_to_odom(x, y, map_coor[p1][0], map_coor[p1][1], p1)
     print(x, y)
     # break
-    navigate(x=x, y=y, yaw=0, speed=0.4, frame="map", stopper=True, id="get_path_nav_"+str(round(rospy.Time.now().to_sec(), 1)))
+    navigate(x=x, y=-y, yaw=0, speed=0.4, frame="map", stopper=True, id="get_path_nav_"+str(round(rospy.Time.now().to_sec(), 1)))
     while True:
         telem = get_telemetry(frame="nav")
         print(telem)
-        if get_dist(x, y, telem.x, telem.y) < 0.05:
+        if get_dist(x, -y, telem.x, telem.y) < 0.05:
             break
