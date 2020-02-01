997a973cf4718cf600e0b66077a891ad0b8e8670
vas59 <vasily.yuryev.a1@gmail.com>
2020-02-01 14:09:58 +0700

    bj

--- src/nav/do_way.py
+++ src/nav/do_way.py
@@ -91,16 +91,16 @@ while True:
 #         break
 print("PATH", path[3:])
 
-for point_name in path[3:]:
-    x, y = tuple(map_coor[point_name])
-    print(x, y)
-    x, y = map_to_odom(x-0.1, y, map_coor[p1][0], map_coor[p1][1], p1)
-    print(x, y)
-    # break
-    navigate(x=x, y=y, yaw=0, speed=0.4, frame="nav", stopper=True, id="get_path_nav_"+str(round(rospy.Time.now().to_sec(), 1)), mode='')
-    while True:
-        telem = get_telemetry(frame="nav")
-        # print(telem)
-        if get_dist(x, y, telem.x, telem.y) < 0.05:
-            break
-    rospy.sleep(5)
+# for point_name in path[3:]:
+#     x, y = tuple(map_coor[point_name])
+#     print(x, y)
+#     x, y = map_to_odom(x-0.1, y, map_coor[p1][0], map_coor[p1][1], p1)
+#     print(x, y)
+#     # break
+#     navigate(x=x, y=y, yaw=0, speed=0.4, frame="nav", stopper=True, id="get_path_nav_"+str(round(rospy.Time.now().to_sec(), 1)), mode='')
+#     while True:
+#         telem = get_telemetry(frame="nav")
+#         # print(telem)
+#         if get_dist(x, y, telem.x, telem.y) < 0.05:
+#             break
+#     rospy.sleep(5)
