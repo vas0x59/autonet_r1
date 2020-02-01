8e85798ff09962e954a2edaf3be34adfe0b09ecc
vas59 <vasily.yuryev.a1@gmail.com>
2020-02-01 12:23:21 +0700

    asdasdasd

--- src/nav/do_way.py
+++ src/nav/do_way.py
@@ -84,8 +84,9 @@ for point_name in path[2:]:
     print(x, y)
     # break
     navigate(x=x, y=y, yaw=0, speed=0.4, frame="nav", stopper=True, id="get_path_nav_"+str(round(rospy.Time.now().to_sec(), 1)), mode='')
-    while True:
-        telem = get_telemetry(frame="nav")
-        # print(telem)
-        if get_dist(x, y, telem.x, telem.y) < 0.05:
-            break
+    # while True:
+    #     telem = get_telemetry(frame="nav")
+    #     # print(telem)
+    #     if get_dist(x, y, telem.x, telem.y) < 0.05:
+    #         break
+    rospy.sleep(5)
